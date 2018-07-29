f5be4c96ccc2f9261a905799d03b88de33b8c998
Luiz Ferrari <lgm.ferrari@gmail.com>
2018-07-29 18:16:27 -0300

    fix(tutorial reducer): change the REMOVE_TUTORIAL handler to work within the immutability concept

--- src/app/reducers/tutorial.reducer.ts
+++ src/app/reducers/tutorial.reducer.ts
@@ -3,19 +3,19 @@ import { Tutorial } from './../models/tutorial.model'
 import * as TutorialActions from './../actions/tutorial.actions'
 
 const initialState: Tutorial = {
-    name: 'Initial Tutorial',
-    url: 'http://google.com'
+	name: 'Initial Tutorial',
+	url: 'http://google.com'
 }
 
 export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
-    switch(action.type) {
-        case TutorialActions.ADD_TUTORIAL:
-            return [...state, action.payload];
+	switch (action.type) {
+		case TutorialActions.ADD_TUTORIAL:
+			return [...state, action.payload];
 
-        case TutorialActions.REMOVE_TUTORIAL:
-            state.splice(action.payload, 1)
-            return state;
-        default:
-            return state;
-    }
+		case TutorialActions.REMOVE_TUTORIAL:
+			const index = action.payload;
+			return [...state.slice(0, index), ...state.slice(index + 1)];
+		default:
+			return state;
+	}
 }
